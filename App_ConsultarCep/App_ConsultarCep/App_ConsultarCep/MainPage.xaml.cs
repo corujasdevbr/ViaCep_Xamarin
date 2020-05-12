0e68c3d4ccb27396cfd5e836b7e9ba78bb09b188
corujasdevbr <fernando.guerra@corujasdev.com.br>
2020-05-12 13:46:41 -0300

    Estrutura Projeto

---
+++ App_ConsultarCep/App_ConsultarCep/App_ConsultarCep/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using App_ConsultarCep.Models;
+using App_ConsultarCep.Services;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace App_ConsultarCep
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            //Validações
+            string cep = txtCep.Text.Trim();
+
+            if (isValidCep(cep))
+            {
+                try
+                {
+                    //Lógica do programa
+                    EnderecoModel endereco = ViaCepService.BuscarCep(cep);
+
+                    if (endereco != null)
+                    {
+                        lblLogradouro.Text = $"Endereço: {endereco.Logradouro},{endereco.Bairro}, {endereco.Localidade}, {endereco.Uf}";
+                    }
+                    else
+                    {
+                        DisplayAlert("Erro crítico", "Cep não encontrado", "Ok");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Erro crítico", ex.Message, "Ok");
+                }
+            }
+        }
+
+        private bool isValidCep(string cep)
+        {
+            if (cep.Length != 8)
+            {
+                DisplayAlert("Erro", "O cep deve conter 8 caracteres", "Ok");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ App_ConsultarCep/App_ConsultarCep/App_ConsultarCep/Services/ViaCepService.cs
@@ -0,0 +1,25 @@
+﻿using App_ConsultarCep.Models;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace App_ConsultarCep.Services
+{
+    public class ViaCepService
+    {
+        private static string enderecoViaCep = "https://viacep.com.br/ws/{0}/json/";
+
+        public static EnderecoModel BuscarCep(string cep)
+        {
+            string urlApi = string.Format(enderecoViaCep, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(urlApi);
+
+            EnderecoModel endereco = JsonConvert.DeserializeObject<EnderecoModel>(conteudo);
+
+            if (endereco.Cep == null) return null;
+
+            return endereco;
+        }
+    }
+}
